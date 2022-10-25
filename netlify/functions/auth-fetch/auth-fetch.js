29755437f1d0bb64bc962af82bb6e2ae4ffb7f46
felipe chernicharo <Felipe.Melo_Chernicharo@genesys.com>
2022-10-24 20:42:04 -0400

    one last try

--- netlify/functions/auth-fetch/auth-fetch.js
+++ netlify/functions/auth-fetch/auth-fetch.js
@@ -1,20 +1,24 @@
 // for a full working demo of Netlify Identity + Functions, see https://netlify-gotrue-in-react.netlify.com/
 
-const fetch = require("node-fetch");
 const { createClient } = require("@supabase/supabase-js");
 
 const { VITE_PROJECT_URL, VITE_ANON_PUB, VITE_SERVICE_ROLE, VITE_SUPABASE_KEY } = process.env;
 
 const handler = async function (event, context) {
   try {
-    // const supabaseAdmin = createClient(VITE_PROJECT_URL, VITE_SUPABASE_KEY);
-    // const supabaseAdmin = createClient(VITE_PROJECT_URL, VITE_SUPABASE_KEY, VITE_SERVICE_ROLE);
-    const supabaseAdmin = createClient(VITE_PROJECT_URL,  VITE_SERVICE_ROLE, {
-      auth: {
-        storageKey: VITE_SUPABASE_KEY
-      },
-      supabaseKey: VITE_SUPABASE_KEY
-    });
+    // const supabaseAdmin = createClient(VITE_PROJECT_URL, VITE_SUPABASE_KEY, {
+    //   service_role: VITE_SERVICE_ROLE,
+    //   service_role_key: VITE_SERVICE_ROLE,
+    //   serviceRoleKey: VITE_SERVICE_ROLE,
+    //   serviceRole: VITE_SERVICE_ROLE,
+    // });
+    const supabaseAdmin = createClient(VITE_PROJECT_URL, VITE_SERVICE_ROLE, VITE_SUPABASE_KEY);
+    // const supabaseAdmin = createClient(VITE_PROJECT_URL,  VITE_SERVICE_ROLE, {
+    //   auth: {
+    //     storageKey: VITE_SUPABASE_KEY
+    //   },
+    //   supabaseKey: VITE_SUPABASE_KEY
+    // });
     // const supabaseAdmin = createClient(VITE_PROJECT_URL, { apiKey: VITE_SUPABASE_KEY, service_role: VITE_SERVICE_ROLE });
     
 
