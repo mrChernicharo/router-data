fdf8524af2244aa96b58201d735c3c73a91c145e
felipe chernicharo <Felipe.Melo_Chernicharo@genesys.com>
2022-10-27 18:54:59 -0400

    better code for toasts

--- src/shared/Toast.jsx
+++ src/shared/Toast.jsx
@@ -1,9 +1,17 @@
 import { createEffect, createMemo, createSignal, onMount } from "solid-js";
 import { createStore, unwrap } from "solid-js/store";
+import { classss } from "../lib/helpers";
 
-const [toastContext, setToastContext] = createStore([
-  // { id: 1, message: "welcome", status: "default", duration: 2000 },
-]);
+const [toastContext, setToastContext] = createStore([]);
+
+const root = document.documentElement;
+
+const COLORS = {
+  success: " bg-success ",
+  warn: " bg-warning ",
+  danger: " bg-error ",
+  default: " bg-base-300 ",
+};
 
 /**
  * @function addToast
@@ -14,30 +22,12 @@ const [toastContext, setToastContext] = createStore([
  * @param {string} info.status
  * @param {number} info.duration
  */
-const addToast = info => {
-  const id = Math.random();
-  const toast = { id, duration: 2000, status: "default", ...info };
-
-  setToastContext(prev => [...prev, toast]);
-
-  console.log("called addToast", unwrap(toastContext));
-
-  setTimeout(() => setToastContext(prev => prev.filter(t => t.id !== id)), toast.duration);
-};
 
 function ToastContainer(props) {
   return (
     <div
       data-component="ToastContainer"
-      style={{
-        position: "fixed",
-        top: "2rem",
-        right: 0,
-        "padding-right": "2rem",
-        "user-select": "none",
-        "z-index": "1000000",
-        "pointer-events": "none",
-      }}
+      class="fixed top-8 right-0 pr-8 select-none pointer-events-none z-[10000]"
     >
       <For each={toastContext}>{toast => <Toast {...toast} />}</For>
     </div>
@@ -46,14 +36,6 @@ function ToastContainer(props) {
 
 function Toast(props) {
   let toastRef;
-  const root = document.documentElement;
-
-  const colors = {
-    success: "green",
-    warn: "goldenrod",
-    danger: "red",
-    default: "#eee",
-  };
 
   onMount(() => {
     root.style.setProperty("--toast-duration", props.duration ?? 2000);
@@ -62,15 +44,13 @@ function Toast(props) {
   return (
     <div
       ref={toastRef}
+      class={classss(
+        "bottom-[100px] p-4 left-1/2 rounded-[10px] z-[10000]",
+        COLORS[props.status] ? COLORS[props.status] : COLORS.default
+      )}
       style={{
         width: "fit-content",
-        bottom: "100px",
-        left: "50%",
-        padding: "1rem",
-        "border-radius": "10px",
         transform: "translateX(-50%)",
-        "z-index": "1000000",
-        background: colors[props.status] ? colors[props.status] : colors.default,
         animation: "render-toaster",
         "animation-duration": props.duration / 1000 + "s",
         "animation-timing-function": "ease",
@@ -83,13 +63,15 @@ function Toast(props) {
   );
 }
 
+const addToast = info => {
+  const id = Math.random();
+  const toast = { id, duration: 2000, status: "default", ...info };
+
+  setToastContext(prev => [...prev, toast]);
+
+  // console.log("called addToast", unwrap(toastContext));
+
+  setTimeout(() => setToastContext(prev => prev.filter(t => t.id !== id)), toast.duration);
+};
+
 export { ToastContainer, addToast };
-
-// width: fit-content;
-// bottom: 100px;
-// left: 50%;
-
-// transform: translateX(-50%);
-// z-index: 1000000;
-// animation: 100s ease 0s 1 normal forwards running render-toaster;
-// background: rgb(238, 238, 238);
